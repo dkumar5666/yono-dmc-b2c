57faaec6706ecf8db88a06e92c61591f2142b7ba
dkumar5666 <146425995+dkumar5666@users.noreply.github.com>
2026-01-20 00:27:07 +0530

    Add redirects to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'yonodmc.in',
+          },
+        ],
+        destination: 'https://www.yonodmc.in/:path*',
+        permanent: true, // 301 redirect
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
